94f008e44eec4e0a04915310c8c8b90cbf1aecca
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-11 13:57:51 +0200

    estructurar trabajo en componentes y editar html

---
+++ src/components/Cabecera.jsx
@@ -0,0 +1,10 @@
+export const Cabecera = () =>{
+  return(
+  <>
+  <header className="cabecera">
+  <h1>Señores que te apuntan con el dedo</h1>
+  <p className="totales"><span className="nmarcados">0</span> señores que te apuntan con el dedo marcados</p>
+  <a className="accion-marcar" href="marcar-todos">Marcar todos</a>
+</header>
+</>)
+}
